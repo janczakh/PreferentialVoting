b7f876a6703eef31b865916c3d6f27e58f49c153
Hubert <H.T.Janczak@student.tudelft.nl>
2022-10-28 19:54:23 +0200

    Created the reader for PreferentialVote

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //Readcsv()
         //RunPreferentialVoting()
-
+        List<PreferentialVote> votes = PreferentialVote.readFile(new File("Votes.csv"));
+        System.out.println(votes);
     }
 }
--- src/PreferentialVote.java
+++ src/PreferentialVote.java
@@ -1,21 +1,42 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.prefs.PreferencesFactory;
 
 public class PreferentialVote {
 
     enum FoodChoice {
-
+        ROTI, GREEK, PIZZA, BURGER, TURKISH, JERUSALEM
     }
     public String voter;
-    public List<String> votes;
+    public List<FoodChoice> votes;
 
-    public PreferentialVote(List<String> votes, String voter) {
+    public PreferentialVote(List<FoodChoice> votes, String voter) {
         this.voter = voter;
         this.votes = votes;
     }
 
-    public PreferentialVote readFile(File file) {
-        return null;
+    @Override
+    public String toString() {
+        return voter + ":" + votes.toString();
+    }
+
+    public static List<PreferentialVote> readFile(File file) throws FileNotFoundException {
+        List<PreferentialVote> votes = new ArrayList<>();
+        Scanner scanner = new Scanner(file);
+        scanner.nextLine(); //skip header
+        while (scanner.hasNextLine()) {
+            List<FoodChoice> foodChoices = new ArrayList<>();
+            List<String> curLine = Arrays.asList(scanner.nextLine().split(";"));
+            for (String choice : curLine.subList(1, curLine.size())) {
+                foodChoices.add(FoodChoice.valueOf(choice.toUpperCase()));
+            }
+            votes.add(new PreferentialVote(foodChoices, curLine.get(0)));
+        }
+        return votes;
     }
 
 }
